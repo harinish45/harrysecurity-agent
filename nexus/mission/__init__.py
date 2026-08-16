65577ebdd5b8baa2bdc3c521a0aa6526bb22d9f2
Harinish <harinishsv@gmail.com>
2026-08-16 21:53:12 +0530

    feat(mission): export mission service API

--- nexus/mission/__init__.py
+++ nexus/mission/__init__.py
@@ -1,5 +1,14 @@
 """Durable mission control primitives for NEXUS-STRIKE."""
 
 from .models import Mission, MissionEvent, MissionStatus, MissionStore
+from .service import MissionService, MissionSummary, default_mission_service
 
-__all__ = ["Mission", "MissionEvent", "MissionStatus", "MissionStore"]
+__all__ = [
+    "Mission",
+    "MissionEvent",
+    "MissionService",
+    "MissionStatus",
+    "MissionStore",
+    "MissionSummary",
+    "default_mission_service",
+]
